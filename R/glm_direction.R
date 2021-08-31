88c11a70ed5cdde4b7fc9043dac462a428fde194
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2021-08-31 14:47:08 -0400

    Fig updates
    Retain fig for all applicable datapoints (don't filter out movers)
    Play with figure and font sizes

--- R/glm_direction.R
+++ R/glm_direction.R
@@ -80,7 +80,7 @@ m_S <- glm(
     commonName:poly(yDay,2),
   data = mdf,
   family = "binomial"
-  )
+)
 
 sjPlot::plot_model(m_S, "pred", terms = c("commonName"))
 sjPlot::plot_model(m_S, "pred", terms = c("yDay"))
@@ -95,7 +95,7 @@ m_N <- glm(
     commonName:poly(yDay,2),
   data = mdf,
   family = "binomial"
-  )
+)
 summary(m_N)
 car::vif(m_N)
 d_N1 <- dredge(m_N)
@@ -148,168 +148,6 @@ rug_df %>% filter(dir != "U") %>%
   dplyr::filter(yDay >= start & yDay <= end) ->
   df_wide_filtered
 
-
-myPlot <- ggplot() +
-  # Plot CI's
-  geom_ribbon(
-    data = df_wide_filtered,
-    aes(x=yDay, ymin = conf.low, ymax = conf.high, fill = mod, group = interaction(Species, mod)),
-    alpha = 0.1
-    ) +
-  geom_path(
-    data = df_wide_filtered,
-    aes(x=yDay, y = conf.low, color = mod, group = interaction(Species, mod) ),
-    linetype = 2
-    ) +
-  geom_path(
-    data = df_wide_filtered,
-    aes(x=yDay, y = conf.high, color = mod, group = interaction(Species, mod) ),
-    linetype = 2
-    ) +
-  # Plot model predictions.
-  geom_path(
-    data = df_wide_filtered,
-    aes(x=yDay, y = predicted, color = mod, group = interaction(Species, mod) ),
-    linetype = 1
-    ) +
-  # Plot rug plots
-  geom_rug(
-    data =  dplyr::filter(rug_df, is_northerly == 1) ,
-     aes(x=yDay),
-     color = col_N,
-     sides = "t"
-  )  +
-  geom_rug(
-    data = dplyr::filter(rug_df, is_southerly == 1) ,
-     aes(x=yDay),
-     color = col_S,
-     sides = "b"
-  )  +
-  # Facet.
-  facet_wrap(~Species) +
-  # Appearance
-  scale_fill_manual(  breaks = c("N", "S"), values = c(col_N, col_S) ) +
-  scale_color_manual( breaks = c("N", "S"), values = c(col_N, col_S) ) +
-  xlab("Day of Year") +
-  ylab("Probability of movement in specified direction") +
-  theme(
-    strip.background = element_rect(fill = "white"),
-    legend.position = "none"
-  )
-
-
-label_df <-
-  data.frame(
-    label = c("North-to-south", "South-to-north"),
-    Species = rep("Hoary", 2),
-    x=c(100,00),
-    y=c(0.9, 0.2),
-    ax1 = c(110, 100),
-    ax2 = c(50,120),
-    ay1 = c(0.88,0.18),
-    ay2 = c(0.8, 0.1)
-    ) %>%
-  dplyr::mutate(
-    Species = factor(Species, levels = c("Hoary", "Eastern red", "Silver-haired"))
-  )
-
-
-(myPlot_labs <-
-  myPlot +
-  geom_text(
-    data = label_df,
-    aes(label = label,x=x,y=y), hjust = 0
-  ) +
-  geom_segment(
-    data = label_df,
-    aes(x=ax1, xend = ax2, y=ay1, yend = ay2)
-  )
-)
-
-ggsave(myPlot_labs, filename = file.path(wd$figs, "directionOfMovementByYday.png"))
-
-
-
-## for only movers -----
-
-
-mdf2 <- mdf %>% filter(dist_km >= 100)
-
-# southerly model
-m_S <- glm(
-  is_southerly ~
-    commonName +
-    poly(yDay,2) +
-    commonName:poly(yDay,2),
-  data = mdf2,
-  family = "binomial"
-)
-
-dredge(m_S)
-car::vif(m_S)
-
-
-m_S2 <- glm(
-  is_southerly ~
-    poly(yDay,2) +
-    commonName:poly(yDay,2),
-  data = mdf2,
-  family = "binomial"
-)
-dredge(m_S2)
-car::vif(m_S2)
-
-sjPlot::plot_model(m_S, "pred", terms = c("yDay"))
-pS2 <- sjPlot::plot_model(m_S, "pred", terms = c("yDay","commonName"))
-pS2
-
-# northerly model
-m_N1 <- glm(
-  is_northerly ~
-    commonName +
-    poly( yDay,2) +
-    commonName:poly(yDay,2),
-  data = mdf2,
-  family = "binomial"
-)
-dredge(m_N1)
-car::vif(m_N1)
-
-sjPlot::plot_model(m_N1, "pred", terms = c("commonName"))
-sjPlot::plot_model(m_N1, "pred", terms = c("yDay"))
-pN1 <-sjPlot::plot_model(m_N1, "pred", terms = c("yDay","commonName"))
-pN1
-
-
-
-col_N <- "#e76f51"
-col_S <- "#023047"
-
-
-df_N <- ggplot_build(pN1)$plot$data %>% as.data.frame %>% mutate(mod = "N")
-df_S <- ggplot_build(pS2)$plot$data %>% as.data.frame %>% mutate(mod = "S")
-df_wide <- rbind(df_N, df_S) %>%
-  dplyr::rename(yDay = x, Species = group) %>%
-  mutate(Species = factor(Species, levels = c("Hoary", "Eastern red", "Silver-haired")))
-
-rug_df <- mdf2 %>%
-  dplyr::select(-c(x, Species)) %>%
-  dplyr::rename(Species = commonName) %>%
-  mutate(Species = factor(Species, levels = c("Hoary", "Eastern red", "Silver-haired")))
-
-# Crop plotted projection to within a certain window of any observations.
-windowToPlot <- 30
-## By species and direction:
-rug_df %>% filter(dir != "U") %>%
-  group_by(Species, dir) %>%
-  dplyr::summarise(min = min(yDay), max= max(yDay)) %>%
-  ungroup %>%
-  mutate(start = min - windowToPlot, end = max + windowToPlot) %>%
-  rename(mod = dir) %>%
-  dplyr::select(Species, mod, start, end) %>%
-  right_join(df_wide, by = c("Species", "mod")) %>%
-  dplyr::filter(yDay >= start & yDay <= end) ->
-  df_wide_filtered
 
 myPlot <- ggplot() +
   # Plot CI's
@@ -376,11 +214,11 @@ label_df <-
   )
 
 
-(myPlot_labs2 <-
+(myPlot_labs <-
     myPlot +
     geom_text(
       data = label_df,
-      aes(label = label,x=x,y=y), hjust = 0
+      aes(label = label,x=x,y=y), hjust = 0, size = 3
     ) +
     geom_segment(
       data = label_df,
@@ -388,4 +226,8 @@ label_df <-
     )
 )
 
-ggsave(myPlot_labs2, filename = file.path(wd$figs, "directionOfMovementByYday_MOVERSONLY.png"))
+ggsave(
+  myPlot_labs,
+  filename = file.path(wd$figs, "directionOfMovementByYday.png"),
+  width = 8, height = 4
+  )
