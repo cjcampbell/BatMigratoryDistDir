94cd91ceaf1e3b54bd2591d24f4d9d1cc0ea21f0
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2021-09-01 17:51:20 -0400

    new plot layout, lots of details

--- R/glm_distTraveled.R
+++ R/glm_distTraveled.R
@@ -6,6 +6,7 @@ library(sjPlot)
 library(car)
 library(MuMIn)
 library(caret)
+library(gtsummary)
 
 mydata_minDistDir <- readRDS(file.path(wd$bin,"mydata_minDistDir.rds") )
 
@@ -40,24 +41,37 @@ dropVIF <- function(vifOUT) {
 
 ggarrange(
   {
-    mdf %>%
+    mydata_minDistDir %>%
+      mutate(didMove = as.numeric(as.factor(didMove))) %>%
       ggplot() +
       aes(didMove) +
       geom_histogram() +
       xlab("Movement > 100km observed")
   }, {
-    mdf %>%
-      filter(didMove == 1) %>%
-    ggplot() +
-    aes(dist_km) +
-    geom_histogram() +
-    xlab("Distance when movement observed")
+    v %>%
+      filter(didMove == "Y") %>%
+      ggplot() +
+      aes(dist_km) +
+      geom_histogram() +
+      xlab("Distance when movement observed")
   }
 )
 
 
 # Model selection ---------------------------------------------------------
 
+mdf <- mydata_minDistDir %>%
+  filter(
+    !is.na(dist_km),
+    !is.na(OriginCluster),
+    !is.na(yDay)
+  ) %>%
+  dplyr::mutate(
+    didMove = case_when(
+      dist  %in% c("mid", "long") ~ 1,
+      dist  %in% c("short")  ~ 0
+    )
+  )
 
 ## Model A -- didMove ----
 m1 <- glm(
@@ -137,8 +151,9 @@ dropVIF(car::vif(m3))
 
 # Lookin good!
 summary(m3)
-plot(m3)
+#plot(m3)
 
+### Plot. -----
 
 list(
   "yday2"                          ,
@@ -151,9 +166,27 @@ list(
   c("yday2", "OriginCluster" )
 ) %>%
   lapply(., function(x) {
-    sjPlot::plot_model(m3, type = "pred", terms = x) + theme_bw()
+    sjPlot::plot_model(m3, type = "pred", terms = x) +
+      coord_cartesian(ylim = c(0,1)) +
+      theme_pubclean() +
+      theme(
+        panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank()
+      )
   } ) -> modPlots1
-gridExtra::grid.arrange(grobs = modPlots1)
+
+#gridExtra::grid.arrange(grobs = modPlots1)
+
+## Model performance. ----
+summary(m3)
+gtsummary::tbl_regression(m3, exponentiate = F) %>% add_q() %>% bold_p(t = 0.10, q = TRUE) %>% italicize_levels()
+performance::r2(m3)
+anova(m3)
+caret::varImp(m3) %>% arrange(desc(Overall))
+
+
+
+
 
 ## Model B -- how far? ----
 
@@ -174,7 +207,7 @@ gl1 <-
       poly(yday2, 2):OriginCluster +
       commonName:poly(yday2, 2):OriginCluster
     ,
-    data = mdf2,
+    data = mdf,
     na.action = "na.fail",
     family = Gamma(link = "log")
   )
@@ -234,10 +267,10 @@ dropVIF(car::vif(gl3))
 car::vif(gl3)
 # A stable model! Woo!!
 
-plot(gl3)
+#plot(gl3)
 summary(gl3)
 
-
+### Plot -----
 
 list(
   "yday2"                          ,
@@ -250,12 +283,204 @@ list(
   c("yday2", "OriginCluster" )
 ) %>%
   lapply(., function(x) {
-    sjPlot::plot_model(gl3, type = "pred", terms = x) + theme_bw()
+    sjPlot::plot_model(gl3, type = "pred", terms = x) +
+      coord_cartesian(ylim = c(0,700)) +
+      theme_pubclean() +
+      theme(
+        panel.grid.minor = element_blank(),
+        panel.grid.major.x = element_blank()
+      )
   } ) -> modPlots2
-gridExtra::grid.arrange(grobs = modPlots2)
+#gridExtra::grid.arrange(grobs = modPlots2)
 
-grid.arrange(
-  arrangeGrob( grobs = modPlots1, ncol = 1 ),
-  arrangeGrob( grobs = modPlots2, ncol = 1 ),
-  ncol = 2
+## Model performance. ----
+# See https://stats.stackexchange.com/questions/431120/how-to-interpret-parameters-of-glm-output-with-gamma-log-link
+summary(gl3)
+gtsummary::tbl_regression(gl3) %>%
+  add_q() %>% bold_p(t = 0.10, q = TRUE) %>% italicize_levels()
+performance::r2(gl3)
+anova(gl3)
+caret::varImp(gl3) %>% arrange(desc(Overall))
+
+
+
+# Bigplot -----------------------------------------------------------------
+## Setup axes. ----
+library(ggpubr)
+
+margins <- list(
+  theme(
+    #/t/r/b/l/
+    plot.margin = unit(c(4, 4, 4, 16), "points"),
+    axis.title = element_text(size = 10)
+  )
 )
+
+blues <- colorRampPalette(c("#03045e", "#00b4d8"))
+OriginClusterColors <- list(
+  scale_color_manual(
+    breaks = 1:4,
+    values = blues(4)
+  ),
+  scale_fill_manual(
+    breaks = 1:4,
+    values = blues(4)
+  )
+)
+speciesColors <- list(
+  scale_color_manual(
+    "Species",
+    breaks = c("Hoary", "Eastern red", "Silver-haired"),
+    values = c("#E09F3E", "#A43828", "#335C67")
+  ),
+  scale_fill_manual(
+    "Species",
+    breaks = c("Hoary", "Eastern red", "Silver-haired"),
+    values = c("#E09F3E", "#A43828", "#335C67")
+  )
+)
+dayOfYear_x <- list(
+  scale_x_continuous(
+    "Day of year",
+    breaks = c(seq(150,365, 50), 365, seq(415, 600, 50)),
+    labels = c(c(150, 200, 250, 300, 350), c( 365,  415, 465, 515, 565)-365)
+  )
+)
+wind_killed <- list(
+  scale_x_continuous(
+    "Wind killed",
+    breaks = c(1,2),
+    labels = c("no", "yes"),
+    expand = c(0.15,0.15)
+  )
+)
+lat <- list(
+  scale_x_continuous(
+    "Sampling latitude"
+  )
+)
+
+species_x <- list(
+  scale_x_continuous(
+    "Species",
+    breaks = c(1,2,3),
+    labels = c("Hoary", "Eastern red", "Silver-haired"),
+    expand = c(0.15,0.15)
+  )
+)
+
+dist_y <- list(
+  scale_y_continuous(
+    "Min. dist (km)",
+    position = "right",
+    breaks = seq(0,750,by=150),
+    labels = seq(0,750,by=150)
+    ),
+  theme(
+    plot.title = element_blank()
+  )
+)
+prob_y <- list(
+  scale_y_continuous(
+    "Prob. of movement",
+    position = "left"
+  ),
+  theme(
+    plot.title = element_blank()
+  )
+)
+
+legendPosition <- "bottom"
+
+## Arrange -----
+myGrobs <- list(
+
+  ggarrange(
+    plotlist = list(
+
+      ggarrange(
+        plotlist = list(
+          modPlots1[[1]] + margins + prob_y + dayOfYear_x,
+          modPlots2[[1]] + margins + dist_y + dayOfYear_x
+        ),
+        labels = c(LETTERS[1:2])
+      ),
+      ggarrange(
+        plotlist = list(
+          modPlots1[[2]] + margins + prob_y ,
+          modPlots2[[2]] + margins + dist_y
+        ),
+        labels = c(LETTERS[3:4])
+      ),
+      ggarrange(
+        plotlist = list(
+          modPlots1[[3]] + margins + prob_y + wind_killed,
+          modPlots2[[3]] + margins + dist_y + wind_killed
+        ),
+        labels = c(LETTERS[5:6])
+      ),
+      ggarrange(
+        plotlist = list(
+          modPlots1[[4]] + margins + prob_y + lat,
+          modPlots2[[4]] + margins + dist_y + lat
+        ),
+        labels = c(LETTERS[7:8])
+      )
+
+    )
+  ),
+
+  ggpubr::ggarrange(
+    plotlist = list(
+      modPlots1[[5]] + margins + prob_y + OriginClusterColors + species_x,
+      modPlots2[[5]] + margins + dist_y + OriginClusterColors + species_x,
+      modPlots1[[8]] + margins + prob_y + OriginClusterColors + dayOfYear_x,
+      modPlots2[[8]] + margins + dist_y + OriginClusterColors + dayOfYear_x
+    ),
+    ncol = 2, nrow = 2, common.legend = T, legend = legendPosition,
+    labels = c(LETTERS[9:12]), hjust=-0.1, vjust = -1
+  ) +
+    theme(plot.margin = margin(1,0,0,0, "cm")) ,
+
+  ggpubr::ggarrange(
+    plotlist = list(
+      modPlots1[[6]] + margins + prob_y + speciesColors + wind_killed,
+      modPlots2[[6]] + margins + dist_y + speciesColors + wind_killed,
+      modPlots1[[7]] + margins + prob_y + speciesColors + lat,
+      modPlots2[[7]] + margins + dist_y + speciesColors + lat
+    ),
+    ncol = 2, nrow = 2, common.legend = T, legend = legendPosition,
+    labels = c(LETTERS[12:15]), hjust=-0.1, vjust = -1
+  ) +
+    theme(plot.margin = margin(1,0,0,0, "cm"))
+
+)
+
+
+
+## Plot -----
+
+# arrangeGrob(
+#   grobs = myGrobs,
+#   layout_matrix = matrix(
+#     data = c(
+#       1,2,
+#       3,4,
+#       5,5,
+#       6,6
+#     ), ncol = 2, nrow = 4, byrow = T
+#   ),
+#   heights = c(1,1,2.5,2.5)
+# ) -> bigP
+#
+# plot(bigP)
+
+arrangeGrob(
+  grobs = myGrobs, ncol=1, nrow= 3
+) -> bigP
+
+## Save ----
+ggsave(bigP, filename = file.path(wd$figs, "distanceModelResults.png"),
+       width = 8, height = 11)
+
+
