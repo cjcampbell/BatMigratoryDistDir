031027521d36d6a0dd97459ccbce94e5effe99ce
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2022-03-24 12:46:48 -0400

    Update 14_dDHistogramPlots.R

--- R/14_dDHistogramPlots.R
+++ R/14_dDHistogramPlots.R
@@ -1,1 +1,16 @@
 mydata_transformed <- readRDS( file.path(wd$bin, "mydata_transformed.rds") )
+library(ggpubr)
+
+p <- mydata_transformed %>%
+  dplyr::mutate(commonName = case_when(Species == "LACI"~ "Hoary", Species == "LABO" ~ "Eastern red", Species == "LANO" ~ "Silver-haired")) %>%
+  dplyr::mutate( commonName = factor(commonName, levels = c("Hoary", "Eastern red", "Silver-haired")) ) %>%
+  ggplot(aes(d2H)) +
+  geom_histogram() +
+  facet_grid(~commonName) +
+  scale_x_continuous(expression(bold(paste(delta^2 ~ H[fur], " (", "\u2030", ", VSMOW)") ))) +
+  scale_y_continuous(expression(bold("Frequency"))) +
+  theme_pubclean() +
+  theme(
+    strip.background = element_rect(fill = "white", color = "white")
+  )
+ggsave(p, filename = file.path(wd$figs, "histogram_dDVals.png"), width = 6, height = 3)
