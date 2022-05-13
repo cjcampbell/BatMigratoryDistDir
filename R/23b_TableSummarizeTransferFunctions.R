f05cfaee763692e07ac76601eb41dd88af7dca9f
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2022-05-13 16:35:08 -0400

    Create 23b_TableSummarizeTransferFunctions.R

---
+++ R/23b_TableSummarizeTransferFunctions.R
@@ -0,0 +1,13 @@
+selectedTransferFunctions <- readRDS(file.path( wd$bin, "sma_selected.rds"))
+resids <- readRDS(file.path(wd$bin, "residual_sumtab.rds"))
+
+selectedTransferFunctions %>%
+  dplyr::select(Species, isoscape, from, to, mean_intercept, mean_slope) %>%
+  dplyr::rename_at(vars(starts_with("mean_")), ~gsub("mean_", "", .x)) %>%
+  full_join(., pivot_wider(resids, names_from = Sourcefile, values_from = sd_resid_bySource) ) %>%
+  mutate(isoscape = case_when(
+    isoscape == "precip_val_66100" ~ "Annual",
+    isoscape == "precip_val_66098"  ~ "June-August"
+  )) %>%
+  write.csv(., file = file.path(wd$figs, "selectedTransferFunctionsTable.csv"), row.names = FALSE)
+
